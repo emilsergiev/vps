1763107516e046cfdb904845e86540a7ffb909bd
Emil Sergiev <emil.sergiev@volasoftware.com>
2020-07-16 19:30:26 +0300

    resolved issue #3 and cleaned up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Profile from './Profile.js'
+import Signin from './Signin.js'
+import { UserSession, AppConfig } from 'blockstack'
+import { Connect } from '@blockstack/connect'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const appConfig = new AppConfig()
+const userSession = new UserSession({ appConfig: appConfig })
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { userData: null }
+
+    this.handleSignOut = this.handleSignOut.bind(this)
+  }
+
+  handleSignOut(e) {
+    e.preventDefault()
+    this.setState({ userData: null })
+    userSession.signUserOut(window.location.origin)
+  }
+
+  render() {
+    const { userData } = this.state
+    const authOptions = {
+      appDetails: {
+        name: 'The View Point Site',
+        icon: window.location.origin + '/icon-192x192.png'
+      },
+      userSession,
+      finished: ({ userSession }) => {
+        this.setState({ userData: userSession.loadUserData() })
+      }
+    }
+    return (
+      <Connect authOptions={authOptions}>
+        { !userData ? <Signin /> : <Profile userData={userData} handleSignOut={ this.handleSignOut } /> }
+      </Connect>
+    )
+  }
+
+  componentDidMount() {
+    if (userSession.isSignInPending()) {
+      userSession.handlePendingSignIn().then((userData) => {
+        window.history.replaceState({}, document.title, "/")
+        this.setState({ userData: userData})
+      });
+    } else if (userSession.isUserSignedIn()) {
+      this.setState({ userData: userSession.loadUserData() })
+    }
+  }
 }
 
-export default App;
+export default App
