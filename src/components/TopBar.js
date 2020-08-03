afbb7d12d568a27132be3681626425969730889e
Emil Sergiev <emil.sergiev@volasoftware.com>
2020-08-03 04:48:39 +0300

    resolved issue #19

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link as RouterLink } from 'react-router-dom'
 import { makeStyles } from '@material-ui/core/styles'
 import { Menu, HomeOutline, LightbulbOutline, Power } from 'mdi-material-ui'
 import {
-  AppBar, Link, Toolbar, Tooltip, IconButton, Typography
+  AppBar, Link, Toolbar, Tooltip, IconButton, Typography,
+  Avatar, Menu as AccountMenu, MenuItem
 } from '@material-ui/core'
+import SettingsOutlinedIcon from '@material-ui/icons/SettingsOutlined'
+import avatarFallbackImage from 'assets/anon.png'
 import { useConnect } from '@blockstack/connect'
+import { PROFILE_URL } from 'assets/constants'
 import Search from 'components/Search'
+import { Person } from 'blockstack'
 
 const useStyles = makeStyles(theme => ({
   home: {
+    flexGrow: 1,
     display: 'none',
     [theme.breakpoints.up('sm')]: {
       display: 'block',
@@ -33,6 +39,20 @@ const useStyles = makeStyles(theme => ({
 const TopBar = (props) => {
   const classes = useStyles()
   const { doOpenAuth } = useConnect()
+  const { userData, handleOpen, toggleTheme, handleSignOut } = props
+
+  const [anchorEl, setAnchorEl] = useState(null)
+  const open = Boolean(anchorEl)
+
+  let avatarImg
+
+  if (userData) {
+    let person = new Person(userData.profile)
+    avatarImg = person.avatarUrl() ? person.avatarUrl() : avatarFallbackImage
+  }
+
+  const handleMenu = (event) => { setAnchorEl(event.currentTarget) }
+  const handleClose = () => { setAnchorEl(null) }
 
   return (
     <AppBar>
@@ -41,40 +61,80 @@ const TopBar = (props) => {
           <IconButton
             edge="start"
             aria-label="Open drawer"
-            onClick={props.handleOpen}
+            onClick={handleOpen}
             className={classes.menuButton}
           >
             <Menu />
           </IconButton>
         </Tooltip>
-        <Typography className={classes.title}>
-          <Tooltip title="Go home" placement="bottom">
-            <IconButton className={classes.home}>
-              <Link color="inherit" component={RouterLink} to="/" >
-                <HomeOutline />
-              </Link>
+        <Typography className={classes.home}>
+          <Tooltip
+            placement="bottom"
+            title={ userData ? 'My home page' : 'Landing page' }
+          >
+            <IconButton
+              component={RouterLink}
+              to={ userData ? userData.username : '/' }
+            >
+              <HomeOutline />
             </IconButton>
           </Tooltip>
         </Typography>
         <Search />
         <Typography className={classes.title}>
           <Tooltip title="Toggle light/dark theme" placement="bottom">
-            <IconButton className={classes.light} onClick={props.toggleTheme}>
+            <IconButton className={classes.light} onClick={toggleTheme}>
               <LightbulbOutline />
             </IconButton>
           </Tooltip>
         </Typography>
         {
-          !props.userData ?
+          userData ?
+          <>
+            <Tooltip title="Profile" placement="bottom">
+              <IconButton
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+              >
+                <Avatar alt="avatar" src={avatarImg} />
+              </IconButton>
+            </Tooltip>
+            <AccountMenu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{vertical: 'top', horizontal: 'right'}}
+              keepMounted
+              transformOrigin={{vertical: 'top', horizontal: 'right'}}
+              open={open}
+              onClose={handleClose}
+            >
+              <MenuItem
+                onClick={handleClose}
+                component={RouterLink}
+                to={userData.username}
+              >
+                <HomeOutline /> &nbsp; My home page
+              </MenuItem>
+              <MenuItem
+                onClick={handleClose}
+                component={Link}
+                color="textPrimary"
+                href={PROFILE_URL}
+                target="_blank"
+                rel="noopener"
+              >
+                <SettingsOutlinedIcon /> &nbsp; Edit my profile
+              </MenuItem>
+              <MenuItem onClick={handleSignOut}>
+                <Power color="secondary" /> &nbsp; Sign Out
+              </MenuItem>
+            </AccountMenu>
+          </>
+          :
           <Tooltip title="Sign In" placement="bottom">
-            <IconButton className={classes.power} onClick={() => doOpenAuth()}>
-              <Power />
-            </IconButton>
-          </Tooltip> :
-          <Tooltip title="Sign Out" placement="bottom">
-            <IconButton className={classes.power} onClick={props.handleSignOut}>
-              <Power color="secondary" />
-            </IconButton>
+            <IconButton onClick={() => doOpenAuth()}><Power /></IconButton>
           </Tooltip>
         }
       </Toolbar>
